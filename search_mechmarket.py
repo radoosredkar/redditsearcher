8796e963c04db7fb521d8430db813273de0edad1
radoosredkar <radoosredkar@hotmail.com>
2020-07-25 20:42:22 +0200

    Add new posts watching to the app

--- search_mechmarket.py
+++ search_mechmarket.py
@@ -1,16 +1,7 @@
 import praw
 import datetime
-from models import SubReddit
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
-engine = create_engine(
-    "sqlite:////home/rado/software/python3/reddit/migrations/app.db", echo=False
-)
-
-
-Sesson = sessionmaker(bind=engine)
-
+from models import SubRedditModel
+from db import Sesson
 
 reddit = praw.Reddit(
     client_id="1EkdAIYXKO82XQ",
@@ -34,43 +25,82 @@ def db_add(found):
     message = found.selftext
     title = found.title
     created = datetime.datetime.fromtimestamp(found.created)
-    subreddit: SubReddit = SubReddit(
-        title=title, date_created=created, description=message
+    flair = found.link_flair_text
+    subreddit: SubRedditModel = SubRedditModel(
+        title=title, date_created=created, description=message, flair=flair
     )
 
     # print(f"X{created}X")
-    existing_sr = sesson.query(SubReddit).filter(SubReddit.date_created
-                                                 ==f"{created}").first()
+    existing_sr = (
+        sesson.query(SubRedditModel)
+        .filter(SubRedditModel.date_created == f"{created}")
+        .first()
+    )
     # print(existing_sr)
     if not existing_sr:
         sesson.add(subreddit)
+
 
 def search():
     subreddit = reddit.subreddit("mechmarket")
     attrs = sorted(subreddit.__dir__())
     # print(subreddit.title)
     all_found = []
-    for submission in subreddit.hot(limit=search_no):
+    for submission in subreddit.stream.submissions():
+        flair: SubmissionFlair = submission.flair
+        print(datetime.datetime.now(), submission.link_flair_text)
         all_found.append(submission)
-    # print([attr for attr in attrs if attr[:1] != '_'])
+        # print([attr for attr in attrs if attr[:1] != '_'])
+        all_found = sorted(all_found, key=lambda x: x.created)
+        for found in all_found:
+            message = found.selftext
+            title = found.title
+            flair = found.link_flair_text
+            created = datetime.datetime.fromtimestamp(found.created)
+            # pAttrs(found)
+            for s in search_for:
+                if s in title.lower():
+                    report[s].append([title, created, flair])
+                    db_add(found)
+            sesson.commit()
+        all_found = []
+        for s, v in report.items():
+            if v:
+                print(s)
+                for t, time, flair in v:
+                    print(time, t, flair)
+                report[s] = []
+
+
+def search_by_num():
+    subreddit = reddit.subreddit("mechmarket")
+    attrs = sorted(subreddit.__dir__())
+    # print(subreddit.title)
+    all_found = []
+    # for submission in subreddit.stream.submissions():
+    for submission in subreddit.new(limit=search_no):
+        print(datetime.datetime.now(), submission.link_flair_text)
+        all_found.append(submission)
+        # print([attr for attr in attrs if attr[:1] != '_'])
     all_found = sorted(all_found, key=lambda x: x.created)
-    print("DB BUILD")
     for found in all_found:
         message = found.selftext
         title = found.title
+        flair = found.link_flair_text
         created = datetime.datetime.fromtimestamp(found.created)
         # pAttrs(found)
         for s in search_for:
             if s in title.lower():
-                report[s].append([title, created])
+                report[s].append([title, created, flair])
                 db_add(found)
-
-
-    sesson.commit()
+        sesson.commit()
+    all_found = []
     for s, v in report.items():
-        print(s)
-        for t, time in v:
-            print(time, t)
+        if v:
+            print(s)
+            for t, time, flair in v:
+                print(time, t, flair)
+            report[s] = []
 
 
 sesson = Sesson()
