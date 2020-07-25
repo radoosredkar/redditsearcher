988ef3df2e5db7cf8daef9aacad7b72c3ee58bf2
radoosredkar <radoosredkar@hotmail.com>
2020-07-25 20:41:00 +0200

    Add flair to model

--- models.py
+++ models.py
@@ -1,16 +1,20 @@
 from sqlalchemy import Integer, String, Column, Date
 from sqlalchemy.ext.declarative import declarative_base
+from db import Sesson
+
 
 Base = declarative_base()
+Base.query = Sesson.query_property()
 
 
-class SubReddit(Base):
+class SubRedditModel(Base):
     __tablename__ = "subreddit"
 
     id = Column(Integer, primary_key=True)
     title = Column(String, index=True)
     description = Column(String)
     date_created = Column(String)
+    flair = Column(String)
 
     def __repr__(self):
-        return f"SubReddit {self.id} {self.title} {self.date_created}"
+        return f"SubReddit {self.id} {self.title} {self.flair} {self.date_created}"
